66ac4afb9032756eb8765084ff6537b38e4ef539
yulinying <yulinying_1994@outlook.com>
2021-06-10 23:08:41 +0800

    init project

---
+++ src/main/java/com/github/alassad/oioda/actions/OpenInOsDefaultAppAction.java
@@ -0,0 +1,55 @@
+package com.github.alassad.oioda.actions;
+
+import com.intellij.ide.actions.RevealFileAction;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.ui.MessageType;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import javax.annotation.Nonnull;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author yulinying
+ * @email yulinying_1994@outlook.com
+ */
+@SuppressWarnings("FieldMayBeFinal")
+public class OpenInOsDefaultAppAction extends AnAction {
+    
+    private static Desktop desktop = Desktop.getDesktop();
+    
+    private static final NotificationGroup notificationGroup = NotificationGroupManager.getInstance()
+            .getNotificationGroup("com.github.alassad.oioda.notify");
+    
+    
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        
+        VirtualFile vFile = RevealFileAction.findLocalFile(event.getData(CommonDataKeys.VIRTUAL_FILE));
+        if (vFile == null) {
+            notify("selected file is empty", MessageType.INFO);
+            return;
+        }
+        File file = new File(vFile.getPath());
+        try {
+            desktop.open(file);
+        } catch (IOException e) {
+            notify("cannot open file", MessageType.ERROR);
+        }
+    }
+    
+    
+    private static void notify(@Nonnull String content, @Nonnull MessageType type) {
+        Notification notification = notificationGroup.createNotification(content, type);
+        Notifications.Bus.notify(notification);
+    }
+    
+    
+}
---
+++ src/main/java/icons/Icons.java
@@ -0,0 +1,15 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * @author yulinying
+ * @email yulinying_1994@outlook.com
+ */
+public interface Icons {
+    
+    Icon DESKTOP_ICON = IconLoader.getIcon("/icons/desktop.png", Icons.class);
+    
+}
